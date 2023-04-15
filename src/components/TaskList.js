aaf2a7e3a36a0ae7db6361e0b395ea2459d462fa
Jeff Ruoss <jeff@zenthree.com>
2023-04-15 12:44:56 -0700

    Change current task and old task background colors.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,7 +4,7 @@ function TaskList({ tasks, currentTask }) {
   return (
     <ul className="space-y-2">
       {tasks.slice().reverse().map((task) => (
-        <li key={task.id} className={`${currentTask === task ? 'bg-green-300' : 'bg-gray-200'} p-4 rounded`}>
+        <li key={task.id} className={`${currentTask === task ? 'bg-green-900' : 'bg-gray-800'} p-4 rounded`}>
           {currentTask === task && <span className="mr-2">[Current Task]</span>}
           <strong>Task:</strong> {task.task} | <strong>Category:</strong> {task.category} |{' '}
           <strong>Start Time:</strong> {task.startTime}
