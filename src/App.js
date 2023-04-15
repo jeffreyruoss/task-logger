e75e015bf94c43aed5e3f3c4b9bd2bbb06331e6f
Jeff Ruoss <jeff@zenthree.com>
2023-04-15 12:27:17 -0700

    Changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   const [tasks, setTasks] = useState([]);
   const [taskInput, setTaskInput] = useState('');
   const [categoryInput, setCategoryInput] = useState('');
+  const [currentTask, setCurrentTask] = useState(null);
+
 
   function handleTaskInputChange(event) {
     setTaskInput(event.target.value);
@@ -26,33 +28,47 @@ function App() {
       task: taskInput,
       category: categoryInput,
       startTime: new Date().toLocaleString(),
+      endTime: null,
     };
   
+    if (currentTask) {
+      currentTask.endTime = new Date().toLocaleString();
+    }
+  
     setTasks([...tasks, newTask]);
+    setCurrentTask(newTask);
     setTaskInput('');
     setCategoryInput('');
   }
+  
 
   return (
-    <div className="App">
-      <h1>Task Log</h1>
-      <input
-        type="text"
-        placeholder="Enter task"
-        value={taskInput}
-        onChange={handleTaskInputChange}
-        onKeyPress={handleKeyPress}
-      />
-      <input
-        type="text"
-        placeholder="Enter task category"
-        value={categoryInput}
-        onChange={handleCategoryInputChange}
-        onKeyPress={handleKeyPress}
-      />
-      <TaskList tasks={tasks} />
+    <div className="bg-gray-900 text-white min-h-screen">
+      <div className="container mx-auto py-10 px-10">
+        <h1 className="text-4xl font-bold mb-6">Task Logger</h1>
+        <div className="flex space-x-4 mb-6">
+          <input
+            type="text"
+            placeholder="Enter task"
+            value={taskInput}
+            onChange={handleTaskInputChange}
+            onKeyPress={handleKeyPress}
+            className="bg-gray-700 text-white placeholder-gray-400 w-full p-2 border border-gray-300 rounded"
+          />
+          <input
+            type="text"
+            placeholder="Enter task category"
+            value={categoryInput}
+            onChange={handleCategoryInputChange}
+            onKeyPress={handleKeyPress}
+            className="bg-gray-700 text-white placeholder-gray-400 w-full p-2 border border-gray-300 rounded"
+          />
+        </div>
+        <TaskList tasks={tasks} currentTask={currentTask} />
+      </div>
     </div>
   );
+  
   
 }
 
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
-function TaskList({ tasks }) {
+function TaskList({ tasks, currentTask }) {
   return (
-    <ul>
+    <ul className="space-y-2">
       {tasks.map((task) => (
-        <li key={task.id}>
+        <li key={task.id} className={`${currentTask === task ? 'bg-green-300' : 'bg-gray-200'} p-4 rounded`}>
+          {currentTask === task && <span className="mr-2">[Current Task]</span>}
           <strong>Task:</strong> {task.task} | <strong>Category:</strong> {task.category} |{' '}
           <strong>Start Time:</strong> {task.startTime}
+          {task.endTime && (
+            <>
+              {' '}
+              | <strong>End Time:</strong> {task.endTime}
+            </>
+          )}
         </li>
       ))}
     </ul>
