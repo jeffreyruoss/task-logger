4b3eb203c39c5cb5f3cd36e0555c2de46327f912
Jeff Ruoss <jeff@zenthree.com>
2023-04-15 12:31:25 -0700

    Reverse order of task list

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,7 +3,7 @@ import React from 'react';
 function TaskList({ tasks, currentTask }) {
   return (
     <ul className="space-y-2">
-      {tasks.map((task) => (
+      {tasks.slice().reverse().map((task) => (
         <li key={task.id} className={`${currentTask === task ? 'bg-green-300' : 'bg-gray-200'} p-4 rounded`}>
           {currentTask === task && <span className="mr-2">[Current Task]</span>}
           <strong>Task:</strong> {task.task} | <strong>Category:</strong> {task.category} |{' '}
