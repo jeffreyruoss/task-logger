c93641900bd68e33e630125ff509b9f69313aeef
Jeff Ruoss <jeff@zenthree.com>
2023-04-21 20:25:27 -0700

    Add and style Stop button, move [Current Task] label, and align task item elements

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -9,33 +9,35 @@ function TaskItem({ task, currentTask, onStopTask }) {
     <li
       className={`${currentTask === task ? "bg-green-900" : "bg-gray-800"} p-4 rounded`}
     >
-      {currentTask === task && (
-        <>
-          <span className="mr-2 text-white">[Current Task]</span>
-          <button
-            onClick={handleStopTask}
-            className="bg-red-500 text-white px-2 py-1 rounded ml-2"
-          >
-            Stop
-          </button>
-        </>
-      )}
-      <div className="flex flex-wrap">
-        <span className="font-semibold mr-1">Task:</span>
-        <span className="mr-4">{task.task}</span>
-        <span className="font-semibold mr-1">Category:</span>
-        <span className="mr-4">{task.category}</span>
-        <span className="font-semibold mr-1">Start Time:</span>
-        <span className="mr-4">
-          {new Date(task.startTime).toLocaleTimeString()}
-        </span>
-        {task.endTime && (
-          <>
-            <span className="font-semibold mr-1">End Time:</span>
-            <span>
-              {new Date(task.endTime).toLocaleTimeString()}
-            </span>
-          </>
+      <div className="flex justify-between items-center">
+        <div className="flex flex-wrap">
+          <span className="font-semibold mr-1">Task:</span>
+          <span className="mr-4">{task.task}</span>
+          <span className="font-semibold mr-1">Category:</span>
+          <span className="mr-4">{task.category}</span>
+          <span className="font-semibold mr-1">Start Time:</span>
+          <span className="mr-4">
+            {new Date(task.startTime).toLocaleTimeString()}
+          </span>
+          {task.endTime && (
+            <>
+              <span className="font-semibold mr-1">End Time:</span>
+              <span>
+                {new Date(task.endTime).toLocaleTimeString()}
+              </span>
+            </>
+          )}
+        </div>
+        {currentTask === task && (
+          <div className="flex items-center">
+            <span className="mr-2 text-white">[Current Task]</span>
+            <button
+              onClick={handleStopTask}
+              className="bg-red-500 text-white px-2 py-1 rounded"
+            >
+              Stop
+            </button>
+          </div>
         )}
       </div>
     </li>
